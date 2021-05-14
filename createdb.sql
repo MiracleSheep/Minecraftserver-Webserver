8149e4e61e30d22cdaa90e5a4b14bad4f5a590f3
MiracleSheep <jpa.khalife@gmail.com>
2021-05-13 21:34:06 -0400

    trying to use docker compose

--- createdb.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE authentication;
-USE authentication;
-CREATE TABLE credentials ( name VARCHAR(20) NOT NULL, username VARCHAR(20) NOT NULL UNIQUE, password VARCHAR(20) NOT NULL UNIQUE, PRIMARY KEY(username));
-CREATE TABLE voting (username VARCHAR(20) NOT NULL, time TIME NOT NULL, date DATE NOT NULL, vote int NOT NULL, PRIMARY KEY(username), FOREIGN KEY(username) REFERENCES credentials(username));
-ALTER TABLE voting ADD UNIQUE diffdate (username,date);
-
-
-
