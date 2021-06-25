3fa39a1cf4ab34bd04f4eba7574e190090273150
John <jpa.khalife@gmail.com>
2021-06-25 12:50:31 -0400

    h

--- init.sql
+++ init.sql
@@ -1,4 +1,3 @@
-CREATE DATABASE authentication;
 USE authentication;
 CREATE TABLE IF NOT EXISTS credentials ( name VARCHAR(20) NOT NULL, username VARCHAR(20) NOT NULL UNIQUE, password VARCHAR(20) NOT NULL UNIQUE, PRIMARY KEY(username));
 CREATE TABLE IF NOT EXISTS voting (username VARCHAR(20) NOT NULL, time TIME NOT NULL, date DATE NOT NULL, vote int NOT NULL, PRIMARY KEY(username), FOREIGN KEY(username) REFERENCES credentials(username));
