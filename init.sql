78e598d295fba5709e51ad0fd9a359be084930ee
John <jpa.khalife@gmail.com>
2021-07-05 09:20:45 -0400

    Chaking mysql to fix init script error

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS authentication;
+SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Custom error';
 USE authentication;
 CREATE TABLE IF NOT EXISTS credentials ( name VARCHAR(20) NOT NULL, username VARCHAR(20) NOT NULL UNIQUE, password VARCHAR(20) NOT NULL, PRIMARY KEY(username));
 CREATE TABLE IF NOT EXISTS voting (username VARCHAR(20) NOT NULL, time TIME NOT NULL, date DATE NOT NULL, vote int NOT NULL, FOREIGN KEY(username) REFERENCES credentials(username));
