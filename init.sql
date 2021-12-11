b6f5ec83f0b7a67b5c18f22072956eccede72199
John <jpa.khalife@gmail.com>
2021-12-11 09:37:48 -0500

    did not specify varaible type

--- init.sql
+++ init.sql
@@ -1,5 +1,5 @@
 USE authentication;
 CREATE TABLE IF NOT EXISTS credentials ( name VARCHAR(20) NOT NULL UNIQUE, username VARCHAR(20) NOT NULL UNIQUE, password VARCHAR(20) NOT NULL, PRIMARY KEY(username));
 CREATE TABLE IF NOT EXISTS voting (username VARCHAR(20) NOT NULL, time TIME NOT NULL, date DATE NOT NULL, vote int NOT NULL, FOREIGN KEY(username) REFERENCES credentials(username));
-CREATE TABLE IF NOT EXISTS birthday (username VARCHAR(20) NOT NULL, userid VARCHAR(20) NOT NULL UNIQUE, date DATE NOT NULL, channelid NOT NULL);
+CREATE TABLE IF NOT EXISTS birthday (username VARCHAR(20) NOT NULL, userid VARCHAR(20) NOT NULL UNIQUE, date DATE NOT NULL, channelid VARCHAR(20) NOT NULL);
 ALTER TABLE voting ADD UNIQUE diffdate (username,date);
